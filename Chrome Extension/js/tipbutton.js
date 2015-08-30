f1be8507a08eec210122cfe5c40262d1c3149119
Joe Looney <joelooney@Joe-Looneys-MacBook-Pro.local>
2015-08-30 11:07:27 -0400

    update payment buttons

--- Chrome Extension/js/tipbutton.js
+++ Chrome Extension/js/tipbutton.js
@@ -1,12 +1,29 @@
 //var address = $(".companion-tip-address").text();
 
-var iconpath = chrome.extension.getURL('pockets-48.png');
+var iconpathblue = chrome.extension.getURL('images/paywithpockets-blue.png');
+var iconpathyellow = chrome.extension.getURL('images/paywithpockets-yellow.png');
+var iconpathgreen = chrome.extension.getURL('images/paywithpockets-green.png');
 var tipsplash = chrome.extension.getURL('tipsplash.html');
 
 $('.pockets-url').html(tipsplash);
-$('.pockets-image').html(iconpath);
+$('.pockets-image').html(iconpathblue);
+$('.pockets-image-yellow').html(iconpathyellow);
+$('.pockets-image-green').html(iconpathgreen);
 
 $('.pockets-payment-button').each(function(i, obj) {
+    
+    var buttoncolor = $(this).attr("data-color");
+
+    if (buttoncolor == "yellow") {
+        var iconcolor = "yellow";
+        iconpath = chrome.extension.getURL('images/paywithpockets-'+iconcolor+'.png');
+    } else if (buttoncolor == "green") {
+        var iconcolor = "green";
+        iconpath = chrome.extension.getURL('images/paywithpockets-'+iconcolor+'.png');
+    } else {
+        var iconcolor = "blue";
+        iconpath = chrome.extension.getURL('images/paywithpockets-'+iconcolor+'.png');
+    }
     
     var address = $(this).attr("data-address");
     var label = $(this).attr("data-label");
@@ -17,7 +34,7 @@ $('.pockets-payment-button').each(function(i, obj) {
     var labelurl = encodeURIComponent(label).replace(/[!'()*]/g, escape);
     var tokensurl = encodeURIComponent(tokens);
     
-    var tipbutton = "<div style='display: inline-block; padding: 5px;'><a href='"+tipsplash+"?address="+address+"&label="+labelurl+"&tokens="+tokensurl+"&amount="+amount+"' target='_blank'><img src='"+iconpath+"' height='24px' width='24px'></a></div>";
+    var tipbutton = "<div style='display: inline-block; padding: 5px;'><a href='"+tipsplash+"?address="+address+"&label="+labelurl+"&tokens="+tokensurl+"&amount="+amount+"' target='_blank'><img src='"+iconpath+"' width='160px'></a></div>";
     
     //"&isxcp="+isxcp+
 
